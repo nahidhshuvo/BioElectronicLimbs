6f4ef2bf1bb6b16876acf312c9633a650323f8f2
Nahid Shuvo <47219761+nahidhshuvo@users.noreply.github.com>
2023-10-30 05:14:44 +0600

    Add files via upload

---
+++ flex_test_by_LED/flex_test_by_LED.ino
@@ -0,0 +1,23 @@
+const int ledPin = 3;  
+const int flexPin = A0;
+
+//Variables:
+int value; //save analog value
+
+void setup(){
+  pinMode(ledPin, OUTPUT);  
+  Serial.begin(9600);      
+}
+
+void loop(){
+  value = analogRead(flexPin);         //Read and save analog value from potentiometer
+                                       //Print value
+  value = map(value, 1017,1023, 0, 255);//Map value 0-1023 to 0-255 (PWM)
+  analogWrite(ledPin, value);          //Send PWM value to led
+   
+  Serial.print("sensor: "); 
+  Serial.print(flexPin); 
+  Serial.print(" value: "); 
+  Serial.println(value);
+  delay(100);                          //Small delay
+}
